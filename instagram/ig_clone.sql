e2f9a0954221708d9eef3989bc8f358fad2a8d55
James Earley <jearley2@wisc.edu>
2018-01-09 06:21:03 +0000

    Section 14 - Instagram Database Schema

---
+++ instagram/ig_clone.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE ig_clone;
+
+USE ig_clone;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(500) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id)
+        REFERENCES users(id),
+    FOREIGN KEY (photo_id)
+        REFERENCES photos(id)
+);
+
+CREATE TABLE likes (
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (photo_id)
+        REFERENCES photos(id),
+    FOREIGN KEY (user_id)
+        REFERENCES users(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+CREATE TABLE follows (
+    followee_id INT NOT NULL,
+    follower_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (followee_id)
+        REFERENCES users(id),
+    FOREIGN KEY (follower_id)
+        REFERENCES users(id),
+    PRIMARY KEY (followee_id, follower_id)
+);
+
+CREATE TABLE tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+    tag_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    FOREIGN KEY (photo_id) REFERENCES photos(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id),
+    PRIMARY KEY (tag_id, photo_id)
+);
